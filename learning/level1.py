5478593703a49ece284d77096ce47523a42dd80b
Gho2t-dev <fabianharrab1@gmail.com>
2025-12-30 20:28:32 +0100

    added more excercises to level1.py

--- learning/level1.py
+++ learning/level1.py
@@ -54,7 +54,7 @@ result = findMaxValue(numbers)
 print(result)
 '''
 
-'level 8 index pos der grössten zahl einer liste finden'
+'''level 8 index pos der grössten zahl einer liste finden'
 numbers = [2, 8, 10, 3, 7]
 
 def IndexOfMaxNum(nums):
@@ -70,3 +70,59 @@ def IndexOfMaxNum(nums):
 
 result = IndexOfMaxNum(numbers)
 print(result)
+'''
+
+
+'''
+'level 9 zweitgrössten wer mit funktion finden'
+
+numbers = [2, 8, 10, 3, 7]
+
+
+
+def findSecondMaxValue(nums):
+    x = nums[0]                     #Startwert und fortlaufend grösste zahl
+    y = nums[0]                     #Startwert und fortlaufend zweitgrösste zahl
+    for i in nums:
+        if i > x:
+            y = x
+            x = i
+        elif i > y and i < x:
+            y = i
+    return y
+
+result = findSecondMaxValue(numbers)
+print(f"The second largest number in the list is: {result}.") 
+'''
+
+# Level 10 Frequency counter, zählen wie oft jedes element vorkommt
+
+nums = [2, 2, 8, 10, 3, 7, 7, 7]
+
+# TODO Input nimmt liste und wandelt alle zahlen in eine dict
+# Idee: Prüfen ob die Zahl schon im dict ist, wenn ja dann count +1 und wenn nein dann neuen index erstellen
+
+def FreqCounter(nums):
+    FreqDict = {}
+    for i in nums:
+        if i in FreqDict:
+            x = FreqDict[i]
+            FreqDict[i] = x + 1
+        else:
+            FreqDict[i] = 1
+    return FreqDict
+
+
+def MostFrequent(count):
+    x = 0
+    y = 0
+    for i in count:
+            if count[i] > x:
+                x = count[i]
+                y = i
+    return y
+
+result = FreqCounter(nums)
+maxCount = MostFrequent(result)
+print(result)
+print(maxCount)
